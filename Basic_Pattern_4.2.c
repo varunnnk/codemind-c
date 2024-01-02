1cc6fbb3d044545e50f0cac11a7f7e48d10dc102
varunnnk <147487760+varunnnk@users.noreply.github.com>
2024-01-02 15:27:14 +0530

    #include<stdio.h> int main() {     int n,i,j,k;     scanf("%d",&n);     k=n;     for(i=1;i<=n;i++)     {         if(i%2!=0)         {             for(j=1;j<=n;j++)             {                 printf("1 ");             }         }         else         {             for(j=1;j<=n;j++)             {                 printf("0 ");             }         }         printf(" ");     } }

---
+++ Basic_Pattern_4.2.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,j,k;
+    scanf("%d",&n);
+    k=n;
+    for(i=1;i<=n;i++)
+    {
+        if(i%2!=0)
+        {
+            for(j=1;j<=n;j++)
+            {
+                printf("1 ");
+            }
+        }
+        else
+        {
+            for(j=1;j<=n;j++)
+            {
+                printf("0 ");
+            }
+        }
+        printf("
+");
+    }
+}
